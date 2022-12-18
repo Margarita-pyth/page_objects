587d7667676868f654c093ed5f1ae0fd15613f49
Margo <m4ngom@yandex.kz>
2022-12-18 12:36:34 +0300

    NoSuchElem exception added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self): 
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(By.ID, 'login_link')
+        except (NoSuchElementException):
+            return False
+        return True
