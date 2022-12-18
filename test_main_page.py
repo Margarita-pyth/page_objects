d6e8048a83dc5401a2fd2f749f3acf2872467f7b
Margo <m4ngom@yandex.kz>
2022-12-18 09:17:17 +0300

    class MainPage added and test_main_page editing completed

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,10 @@
-import time
-
-from selenium.webdriver.common.by import By
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link) 
-    go_to_login_page(browser)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 # pytest -v --tb=line --language=en test_main_page.py
