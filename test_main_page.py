3c5051732abcb80f9870900b92d10532911e0409
Margo <m4ngom@yandex.kz>
2022-12-18 08:49:54 +0300

    class BasePage added

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,14 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link) 
+    go_to_login_page(browser)
 
 # pytest -v --tb=line --language=en test_main_page.py
