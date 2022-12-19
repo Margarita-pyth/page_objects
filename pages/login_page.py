08634f2fee43d933d6051741082817f16366ff03
Margo <m4ngom@yandex.kz>
2022-12-19 12:38:33 +0300

    added checks for login_page

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "There is no Login substring in the current url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login Form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Registration Form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -12,5 +13,23 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_see_the_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                       # открываем страницу с формой залогина и регистрации
+    page.should_be_login_form()       # проверка что гость видит форму для залогина
+
+def test_guest_can_see_the_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                       # открываем страницу с формой залогина и регистрации
+    page.should_be_register_form()    # проверка что гость видит форму для регистрации
+
+def test_exists_login_in_current_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                       # открываем страницу с формой залогина и регистрации
+    page.should_be_login_url()        # проверка что url содержит подстроку "login"
 
 # pytest -v --tb=line --language=en test_main_page.py
